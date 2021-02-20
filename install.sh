79fb76d6613bd11b525aafadd29a00835cbd22f0
Silvère Marie <silvere@live.fr>
2021-02-20 18:06:43 +0100

    add docker

--- install.sh
+++ install.sh
@@ -11,6 +11,16 @@ sudo apt-get update -y
 sudo apt-get install google-chrome-stable -y
 
 
+echo "Docker installation..."
+sudo apt-get update -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 if [ -d ~/.local/share/JetBrains/Toolbox ]; then
     echo "JetBrains Toolbox is already installed!"
