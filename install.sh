c3d4dceff76a1c3f10cf2f453f1077188cc8f26f
Silvère Marie <silvere@live.fr>
2021-02-20 14:24:22 +0100

    initialize repository

---
+++ install.sh
@@ -0,0 +1,33 @@
+sudo apt upgrade -y && sudo apt update -y
+sudo apt install git curl -y --fix-missing
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+curl -s "https://get.sdkman.io" | bash 
+
+
+echo "Chrome installation..."
+sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt-get update -y
+sudo apt-get install google-chrome-stable -y
+
+
+
+if [ -d ~/.local/share/JetBrains/Toolbox ]; then
+    echo "JetBrains Toolbox is already installed!"
+    exit 0
+fi
+
+echo "JetBrains Toolbox installation..."
+
+wget --show-progress -qO ./toolbox.tar.gz https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-1.14.5179.tar.gz
+
+TOOLBOX_TEMP_DIR=$(mktemp -d)
+
+tar -C "$TOOLBOX_TEMP_DIR" -xf toolbox.tar.gz
+rm ./toolbox.tar.gz
+
+"$TOOLBOX_TEMP_DIR"/*/jetbrains-toolbox
+
+rm -r "$TOOLBOX_TEMP_DIR"
+
+echo "JetBrains Toolbox was successfully installed!"
