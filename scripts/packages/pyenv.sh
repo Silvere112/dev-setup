cee9b552237d1fb4eb9b0074a95d5ea1597f3fc6
Silvère Marie <silvere@live.fr>
2021-02-27 22:30:03 +0100

    fix(pyenv): add bashrc config

--- scripts/packages/pyenv.sh
+++ scripts/packages/pyenv.sh
@@ -1,1 +1,9 @@
+if [ -d ~/.pyenv ]; then
+    echo "Pyenv already installed!"
+    exit 0
+fi
+
 curl https://pyenv.run | bash
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
