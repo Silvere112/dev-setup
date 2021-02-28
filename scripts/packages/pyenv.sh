969acb595d8b6aa5c7704e93633d3f33bdd07d2b
Silvère Marie <silvere@live.fr>
2021-02-28 09:52:13 +0100

    fix(pyenv): add missing dependencies

--- scripts/packages/pyenv.sh
+++ scripts/packages/pyenv.sh
@@ -3,6 +3,10 @@ if [ -d ~/.pyenv ]; then
     exit 0
 fi
 
+sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+
 curl https://pyenv.run | bash
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
