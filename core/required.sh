f532e29f532170ae67441a8466043bc8aa3a2e9d
Silvère Marie <silvere@live.fr>
2021-03-30 19:12:40 +0200

    feat(core): remove dependencies and update

--- core/required.sh
+++
@@ -1,6 +0,0 @@
-source "${BASH_SOURCE%/*}/scripts.sh"
-
-install_required() {
-  install_script "${BASH_SOURCE%/*}/../$required_scripts_folder/update.sh"
-  install_script "${BASH_SOURCE%/*}/../$required_scripts_folder/dependencies.sh"
-}
