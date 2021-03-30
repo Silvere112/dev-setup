8efa1e18037de25d1a87c98703c1b41d60802e14
Silvère Marie <silvere@live.fr>
2021-03-30 22:06:45 +0200

    feat(core): add homebrew command & script

--- core/packages.sh
+++ core/packages.sh
@@ -1,27 +1,9 @@
 source "${BASH_SOURCE%/*}/scripts.sh"
-source "${BASH_SOURCE%/*}/constants.sh"
+source "${BASH_SOURCE%/*}/common.sh"
+source "${BASH_SOURCE%/*}/brew.sh"
 
 BASE_FOLDER="${BASH_SOURCE%/*}/.."
 
-confirm_and_install(){
-  local packages=$1
-  read -r -p "Do you want to install $packages? [y/N] " response
-  if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-  then
-    install "$packages"
-  fi
-}
-
-install(){
-    local packages=$1
-    validate_packages "$packages"
-    install_packages "$packages"
-}
-
-list_all_packages() {
-  local scripts_path=("$BASE_FOLDER/$package_scripts_folder"/*."$script_extension")
-  packages_name_of "${scripts_path[@]}"
-}
 
 install_packages() {
   local packages=$*
@@ -33,39 +15,11 @@ install_packages() {
 
 install_package() {
   local package=$1
-  install_script "$BASE_FOLDER/$package_scripts_folder/$package.$script_extension"
-}
-
-package_name_of() {
-  local script_path=$1
-  local script_name="$(basename "$script_path")"
-  local script_display_name="${script_name%.*}"
-  echo "$script_display_name"
-}
-
-packages_name_of() {
-  local script_paths=$1
-  local packages_name=""
-  for script_path in "${scripts_path[@]}"; do
-    packages_name+="$(package_name_of "$script_path") "
-  done
-  echo "$packages_name"
-}
-
-
-validate_packages(){
-  local packages=$*
-  for package in $packages; do
-      verify_package "$package"
-  done
-}
-
-verify_package(){
-  local package=$1
-  if [ ! -f "$BASE_FOLDER/$package_scripts_folder/$package.$script_extension" ]; then
-    echo "Unknown package $package"
-    echo "$help"
-    exit 1
+  if  is_brew_package "$1" ;
+  then
+      install_using_brew "$(echo "$package" | sed "s/$brew_command//")"
+  else
+      install_using_script "$BASE_FOLDER/$package_scripts_folder/$package.$script_extension"
   fi
 }
 
--- main.sh
+++ main.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 set -u -e
 
-source "${BASH_SOURCE%/*}/core/constants.sh"
+source "${BASH_SOURCE%/*}/core/common.sh"
 source "${BASH_SOURCE%/*}/core/packages.sh"
 
 if [ $# -eq 1 ] && [ "$1" = "list" ]; then
-  list_all_packages
+  list_all_scripts
   exit 0
 fi
 
-if [ $# -eq 1 ] && [ "$1" = "install" ] ; then
-  confirm_and_install "$(list_all_packages)"
+if [ $# -eq 2 ] && [ "$1" = "list" ]; then
+  list_packages_in "$2"
   exit 0
 fi
 
 if [ $# -eq 3 ] && [ "$1" = "install" ] && [ "$2" = "setup" ] ; then
-  install "$(list_packages_in "$3")"
+  install_packages "$(list_packages_in "$3")"
   exit 0
 fi
 
 if [ $# -gt 1 ] && [ "$1" = "install" ]; then
-  install "${@:2}"
+  install_packages "${@:2}"
   exit 0
 fi
 
