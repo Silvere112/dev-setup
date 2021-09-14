aa3cf984662fe428adacadb6db3f400ab4dcc8c9
Silvère Marie <silvere@live.fr>
2021-09-14 18:52:03 +0200

    fix: update docker installation

--- main/scripts/packages/docker.sh
+++ main/scripts/packages/docker.sh
@@ -1,10 +1,3 @@
-sudo apt-get update -y
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo usermod -aG docker $USER
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh ./get-docker.sh
+rm get-docker.sh
