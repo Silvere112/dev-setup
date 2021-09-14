a99c7695e92a7796aaa93d9ef25cdb697a1baee6
Silvère Marie <silvere@live.fr>
2021-09-14 15:50:28 +0200

    fix: add missing dependency for pyenv

--- main/scripts/packages/pyenv.sh
+++ main/scripts/packages/pyenv.sh
@@ -5,7 +5,7 @@ fi
 
 sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl git zlib1g-dev
 
 curl https://pyenv.run | bash
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
