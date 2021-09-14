920fee5069c125c7d1bcf216e378be1766798a27
Silvère Marie <silvere@live.fr>
2021-09-14 15:10:43 +0200

    fix: add auto approve git install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e -u -x
-sudo apt-get install git
+sudo apt-get install -y git
 TEMP_DIR=$(mktemp -d)
 git clone https://github.com/Silvere112/dev-setup.git "$TEMP_DIR"
 bash -c "$TEMP_DIR/main.sh install setup $1"
